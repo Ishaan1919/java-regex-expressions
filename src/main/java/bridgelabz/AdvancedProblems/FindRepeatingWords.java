0f1afbadb00cfe219cd9caa0c8c0cc499d33a4ab
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-04-02 10:10:20 +0530

    Program to extract the repeated words in the sentence using regex expression

---
+++ src/main/java/bridgelabz/AdvancedProblems/FindRepeatingWords.java
@@ -0,0 +1,17 @@
+package bridgelabz.AdvancedProblems;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FindRepeatingWords {
+    public static void main(String[] args) {
+        String regex = "([\\w]+)(\\s+)(\\1)";
+        String test = "This is is a repeated repeated word test.";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(test);
+
+        while(matcher.find()){
+            System.out.println("Extracted repeated words ✅: " + matcher.group());
+        }
+    }
+}
